f83b328a19cfbe62df70fc2a2fb2d32fd0830212
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-04-28 23:16:20 +0200

    Added template.

---
+++ template/php/email_config.php
@@ -0,0 +1,9 @@
+<?php
+	$to_email = 'c.vazquezlosada@gmail.com';
+	$subject = 'Contact from carlosvlosada.com';	
+	
+	$invalid_name = 'Please enter your name.';
+	$invalid_email = 'Please enter valid e-mail.';
+	$invalid_message = 'Please enter your message.';
+    $sending_error = 'Sorry, we can\'t send your message.';
+?>
