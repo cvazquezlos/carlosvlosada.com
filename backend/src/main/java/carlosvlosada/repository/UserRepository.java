7ef2abab81d66e943495484a908c12f32a264f42
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-05-19 22:02:25 +0200

    Updated UserService and UserRepository methods.

--- backend/src/main/java/carlosvlosada/repository/UserRepository.java
+++ backend/src/main/java/carlosvlosada/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package carlosvlosada.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import carlosvlosada.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	
+	List<User> findByNickName(String nickName);
 }
--- backend/src/main/java/carlosvlosada/service/UserService.java
+++ backend/src/main/java/carlosvlosada/service/UserService.java
@@ -1,8 +1,20 @@
 package carlosvlosada.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import carlosvlosada.model.User;
+import carlosvlosada.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findByNickName(String nickName) {
+		return userRepository.findByNickName(nickName);
+	}
 }
