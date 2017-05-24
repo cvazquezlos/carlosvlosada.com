7d522ed75c61a16118b0589e34ab328118b9982d
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-05-24 19:21:30 +0200

    Updated Main controller and Item service and repository methods.

--- backend/src/main/java/carlosvlosada/controller/Main.java
+++ backend/src/main/java/carlosvlosada/controller/Main.java
@@ -1,8 +1,13 @@
 package carlosvlosada.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import carlosvlosada.model.Item;
 import carlosvlosada.service.CommentService;
 import carlosvlosada.service.ItemService;
 import carlosvlosada.service.PostService;
@@ -19,4 +24,13 @@ public class Main {
 	private PostService postService;
 	@Autowired
 	private UserService userService;
+
+	@RequestMapping("")
+	public String home(Model model) {
+
+		List<Item> portfolio = itemService.findAll();
+		model.addAttribute("items", portfolio);
+
+		return "index";
+	}
 }
--- backend/src/main/java/carlosvlosada/service/ItemService.java
+++ backend/src/main/java/carlosvlosada/service/ItemService.java
@@ -3,6 +3,8 @@ package carlosvlosada.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import carlosvlosada.model.Item;
@@ -14,7 +16,19 @@ public class ItemService {
 	@Autowired
 	private ItemRepository itemRepository;
 
+	public List<Item> findAll() {
+		return itemRepository.findAll();
+	}
+
+	public Page<Item> findAll(int page) {
+		return itemRepository.findAll(new PageRequest(page, 3));
+	}
+
 	public List<Item> findByCategory(String category) {
 		return itemRepository.findByCategory(category);
+	}
+	
+	public Item findOne(int id) {
+		return itemRepository.findOne(id);
 	}
 }
