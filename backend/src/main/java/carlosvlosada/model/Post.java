363dee24b9af1e480ecd9936ceec8c70315f93ca
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-05-25 12:16:30 +0200

    Updated Post entity and Post template.

--- backend/src/main/java/carlosvlosada/model/Post.java
+++ backend/src/main/java/carlosvlosada/model/Post.java
@@ -1,8 +1,11 @@
 package carlosvlosada.model;
 
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -11,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
+import org.joda.time.DateTime;
 
 @Entity
 public class Post {
@@ -22,6 +27,9 @@ public class Post {
 	private String title;
 	private String littleImage;
 	private Date creationDate;
+	private int day;
+	private String month;
+	private int year;
 	private List<Object> content;
 
 	@ManyToOne
@@ -34,6 +42,11 @@ public class Post {
 		this.title = title;
 		this.littleImage = littleImage;
 		this.creationDate = creationDate;
+		DateTime dateTime = new DateTime(this.creationDate);
+		this.day = dateTime.getDayOfMonth();
+		int month = dateTime.getMonthOfYear();
+		this.month = Month.of(month).getDisplayName(TextStyle.SHORT_STANDALONE, new Locale("es", "ES"));
+		this.year = dateTime.getYear();
 		this.content = new ArrayList<Object>(content);
 		this.writer = writer;
 		this.comments = new ArrayList<Comment>();
@@ -69,6 +82,30 @@ public class Post {
 
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
 	}
 
 	public List<Object> getContent() {
